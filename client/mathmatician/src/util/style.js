86c3e0bec0a8eba62d5deb8636fbee8c2a9d53be
minjeoong <orange_mj@naver.com>
2023-09-24 21:41:19 +0900

    feat: color/images 초기설정 추가

---
+++ client/mathmatician/src/util/style.js
@@ -0,0 +1,11 @@
+import { css } from "@emotion/css";
+
+export const Colors = {
+  Black: "#222222",
+  Gray: "#A0A0A0",
+  Red: "#FE5d5D",
+};
+
+export const Images = {
+  flower_above: "/assets/images/flower_above.svg",
+};
