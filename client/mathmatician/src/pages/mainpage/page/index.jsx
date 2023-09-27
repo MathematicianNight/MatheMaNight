1439888ab9cbc7adfd010c812244119fc3312355
minjeoong <orange_mj@naver.com>
2023-09-27 20:40:52 +0900

    feat: mapfunction /hellokitty 페이지로 이동

--- client/mathmatician/src/pages/mainpage/page/index.jsx
+++ client/mathmatician/src/pages/mainpage/page/index.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 
 import { MainpageContainer } from "./styles";
-import Mapfunction from "../component/mapComponent/index";
 
 const mainPage = () => {
   return (
     <MainpageContainer>
       여기가 mainpage
-      <div className="Container">
-        <Mapfunction />
-      </div>
+      <div className="Container"></div>
     </MainpageContainer>
   );
 };
--- client/mathmatician/src/pages/test/hellokitty.jsx
+++ client/mathmatician/src/pages/test/hellokitty.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Mapfunction from "../mainpage/component/mapComponent/index";
 
 const mainPage = () => {
-  return <div>여기가 hellokitty</div>;
+  return (
+    <div>
+      여기가 hellokitty
+      <Mapfunction />
+    </div>
+  );
 };
 
 export default mainPage;
