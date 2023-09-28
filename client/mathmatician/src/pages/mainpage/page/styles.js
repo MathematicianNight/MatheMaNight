da2d25eac4b379e977ab0d8028665048205ee4bd
hyellokitty <hyelee1218@gmail.com>
2023-09-28 21:03:22 +0900

    feat: 배경 (아직은 임시로지만) 설정 완료

--- client/mathmatician/src/pages/mainpage/page/styles.js
+++ client/mathmatician/src/pages/mainpage/page/styles.js
@@ -1,8 +1,26 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
+import { Colors, Images } from '../../../utils/style';
 
 export const MainpageContainer = styled.section`
-  color: #fff;
-  & > .Container {
-    // @todo container 코드 작성
+  width: ${window.innerWidth}px;
+  height: ${window.innerHeight}px;
+  margin: 0 auto;
+  background-image: linear-gradient(180deg, #0F0019 0%, #140065 48.26%, #987FFF 100%);
+  color: #FFFFFF;
+
+  overflow: auto scroll;
+  scroll-snap-type: y mandatory;
+
+  .parent {
+    height: 4220px;
+    text-align: center;
   }
+
+  .child {
+    height: ${window.innerHeight}px;
+    font-size: 50px;
+    line-height: ${window.innerHeight}px;
+    scroll-snap-align: start;
+  }
+
 `;
