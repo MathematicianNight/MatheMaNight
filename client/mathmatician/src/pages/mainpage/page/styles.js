5a0a36640d0cd5c4d0b5d09114af5048ae2ab516
hyellokitty <126170248+hyellokitty@users.noreply.github.com>
2023-09-28 21:19:53 +0900

    feat: 배경 (아직은 임시로지만) 설정 완료

--- client/mathmatician/src/pages/mainpage/page/styles.js
+++ client/mathmatician/src/pages/mainpage/page/styles.js
@@ -2,25 +2,19 @@ import styled from '@emotion/styled';
 import { Colors, Images } from '../../../utils/style';
 
 export const MainpageContainer = styled.section`
-  width: ${window.innerWidth}px;
+  width: 100%;
   height: ${window.innerHeight}px;
   margin: 0 auto;
   background-image: linear-gradient(180deg, #0F0019 0%, #140065 48.26%, #987FFF 100%);
   color: #FFFFFF;
-
+  text-align: center;
   overflow: auto scroll;
   scroll-snap-type: y mandatory;
 
-  .parent {
-    height: 4220px;
-    text-align: center;
-  }
-
   .child {
-    height: ${window.innerHeight}px;
-    font-size: 50px;
+    height: 100%;
+    font-size: 2rem;
     line-height: ${window.innerHeight}px;
     scroll-snap-align: start;
   }
-
 `;
