81333512c4126e21d7150cde0dd2ce14495d079e
LSuhyeon33 <gus9895@naver.com>
2023-09-30 18:51:28 +0900

    api swagger 작성 및 구조 변경

--- server/lib/db.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql2');
-var db = mysql.createConnection({
-  nickname: '',
-  question: '',
-  answer: '',
-});
-db.connect();
-
-module.exports = db;
--- server/lib/sessionOption.js
+++
@@ -1,7 +0,0 @@
-// MySQL에 저장할 세션의 옵션
-var options = {
-  nickname: '',
-  question: '',
-  answer: '',
-  port: 3306,
-};
