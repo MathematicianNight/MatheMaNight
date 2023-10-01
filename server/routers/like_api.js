16bfe3c8445f425d32cb48fec677889677dd16ec
LSuhyeon33 <gus9895@naver.com>
2023-10-01 16:52:57 +0900

    gitignore 수정

--- server/routers/like_api.js
+++ server/routers/like_api.js
@@ -3,10 +3,39 @@ const router = express.Router();
 
 let likeCnt = 0;
 
+/**
+ * @swagger
+ * paths:
+ *  /like:
+ *    get:
+ *      tags: [Like]
+ *      summary: "좋아요 개수 조회"
+ *      description: "서버에 데이터를 보내지 않고 Get방식으로 요청"
+ *      responses:
+ *        "200":
+ *          description: 좋아요 정보
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: integer
+ *                description: "좋아요 개수"
+ *                example: 193
+ */
 router.get('/', (req, res) => {
   res.json(likeCnt);
 });
 
+/**
+ * @swagger
+ * paths:
+ *  /like/upcount:
+ *    post:
+ *      tags: [Like]
+ *      summary: "좋아요 개수 증가"
+ *      description: "POST 방식으로 좋아요 개수 증가"
+ *      requestBody:
+ *        required: false
+ */
 router.post('/upcount', (req, res) => {
   likeCnt++;
   res.json('likeCnt is update');
