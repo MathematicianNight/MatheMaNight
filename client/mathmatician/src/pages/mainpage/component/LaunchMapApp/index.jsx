71d4bab247207992544df78f578c1b32c27cd1e0
minjeoong <orange_mj@naver.com>
2023-10-05 00:10:37 +0900

    feat: greetings 구현

--- client/mathmatician/src/pages/mainpage/component/LaunchMapApp/index.jsx
+++ client/mathmatician/src/pages/mainpage/component/LaunchMapApp/index.jsx
@@ -1,45 +1,48 @@
 import { useState, useEffect } from "react";
 import MapContainer from "./style";
-import { Images, Colors } from "../../../../util/style";
+import { Images, Colors } from "../../../../utils/style";
 
 const openGoogleMap = () => {
   setTimeout(() => {
-    window.location.href = "https://www.google.com/maps/search/?api=1&query=더블유파티";
+    window.location.href =
+      "https://www.google.com/maps/search/?api=1&query=더블유파티";
   }, 900);
-}
+};
 const openNaverMap = () => {
   setTimeout(() => {
     // window.location.href = "https://m.map.naver.com/map.naver?pinId=35643864";
-    window.location.href = "https://m.map.naver.com/search2/site.naver?query=더블유파티&sm=hty&style=v5&code=35643864";
+    window.location.href =
+      "https://m.map.naver.com/search2/site.naver?query=더블유파티&sm=hty&style=v5&code=35643864";
   }, 800);
-}
+};
 const openKakaoMap = () => {
   setTimeout(() => {
     window.location.href = "https://map.kakao.com/link/map/25779135";
   }, 900);
-}
+};
 
 const fallbackCopyClipBoard = (content) => {
-  const textArea = document.createElement('textarea');
+  const textArea = document.createElement("textarea");
   try {
     textArea.value = content;
     document.body.appendChild(textArea);
-    textArea.select();        
-    const successful = document.execCommand('copy');
+    textArea.select();
+    const successful = document.execCommand("copy");
     if (successful) {
-      alert('주소가 복사되었습니다.');
+      alert("주소가 복사되었습니다.");
     }
   } catch (err) {
-    alert('클립보드 복사를 지원하지 않는 브라우저입니다.');
+    alert("클립보드 복사를 지원하지 않는 브라우저입니다.");
   }
   document.body.removeChild(textArea);
 };
 
 const copyClipBoard = (content) => {
   if (navigator.clipboard) {
-    navigator.clipboard.writeText(content)
+    navigator.clipboard
+      .writeText(content)
       .then(() => {
-        alert('주소가 복사되었습니다.');
+        alert("주소가 복사되었습니다.");
       })
       .catch((err) => {
         fallbackCopyClipBoard(content);
@@ -56,29 +59,28 @@ const LaunchMapApp = () => {
     if (isBlurred) {
       setTimeout(() => {
         setIsBlurred(!isBlurred);
-      }, 400)
+      }, 400);
     } else {
       setIsBlurred(!isBlurred);
     }
-  }
+  };
 
   return (
-    <MapContainer
-      isBlurred={isBlurred}
-      onClick={showIcons}
-    >
-      <img 
-        className="map-app-background"
-        src={Images.location_drawing}
-      />
+    <MapContainer isBlurred={isBlurred} onClick={showIcons}>
+      <img className="map-app-background" src={Images.location_drawing} />
       <div className="open-map-app">
-          <img src={Images.google_map_icon} onClick={openGoogleMap} />
-          <img src={Images.naver_map_icon} onClick={openNaverMap} />
-          <img src={Images.kakao_map_icon} onClick={openKakaoMap} />
+        <img src={Images.google_map_icon} onClick={openGoogleMap} />
+        <img src={Images.naver_map_icon} onClick={openNaverMap} />
+        <img src={Images.kakao_map_icon} onClick={openKakaoMap} />
       </div>
       <div className="copy-addr">
         <span>서울 성북구 동소문로 284 길음 서희스타힐스</span>
-        <span className="copy-addr-button" onClick={() => copyClipBoard('서울 성북구 동소문로 284 길음 서희스타힐스')}>
+        <span
+          className="copy-addr-button"
+          onClick={() =>
+            copyClipBoard("서울 성북구 동소문로 284 길음 서희스타힐스")
+          }
+        >
           <img src={Images.copy_addr_icon} /> 복사
         </span>
       </div>
--- client/mathmatician/src/pages/test/hellokitty.jsx
+++ client/mathmatician/src/pages/test/hellokitty.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import LaunchMapApp from "../mainpage/component/LaunchMapApp/index";
 import LaunchCalendarApp from "../mainpage/component/LaunchCalendarApp/index";
-import resizeViewportHeight from "../../util/resizeViewportHeight";
+import resizeViewportHeight from "../../utils/resizeViewportHeight";
 import MainpageContainer from "./hellokitty_style";
 import QnaComponent from "./../mainpage/component/Qna/index";
 
@@ -13,15 +13,15 @@ const Hellokitty = () => {
 
   useEffect(() => {
     resizeViewportHeight();
-    window.addEventListener('resize', resizeViewportHeight);
-    return () => window.removeEventListener('resize', resizeViewportHeight);
+    window.addEventListener("resize", resizeViewportHeight);
+    return () => window.removeEventListener("resize", resizeViewportHeight);
   }, []);
   /* useEffect(() => {
     if (typeof window !== 'undefined') {
       setViewportY(window.innerHeight);
     }
   }, []); */
-  
+
   // css로 안 주고 직접 스크롤 기능 구현하다가 실패...
   /* const handleScroll = (e) => {
     let nextY = e.target.scrollTop;
@@ -46,9 +46,7 @@ const Hellokitty = () => {
   return (
     // <MainpageContainer onScroll={handleScroll}>
     <MainpageContainer>
-      <div className="mainpage-section home-layout" >
-        {/* fill in ... */}
-      </div>
+      <div className="mainpage-section home-layout">{/* fill in ... */}</div>
 
       <div className="mainpage-section greetings-layout">
         {/* fill in ... */}
@@ -57,33 +55,28 @@ const Hellokitty = () => {
       <div className="mainpage-section details-layout">
         <div className="information-wrapper">
           <div className="information-title">Information</div>
-          <div>
-            
-          </div>
+          <div></div>
         </div>
         <div className="program-wrapper">
           <div className="program-title">Program</div>
-
         </div>
       </div>
 
       <div className="mainpage-section morefunc-layout">
         <div className="morefunc-wrapper">
           <div className="morefunc-title">More Functions</div>
-          <div className="morefunc-description">위젯을 클릭하여 일정을 등록하고, 지도 앱을 통해 위치를 확인해 보세요.</div>
+          <div className="morefunc-description">
+            위젯을 클릭하여 일정을 등록하고, 지도 앱을 통해 위치를 확인해
+            보세요.
+          </div>
           <div className="morefunc-widgets-wrapper">
             <div className="later1">헤헿</div>
             <div className="later2">헤헿</div>
             <LaunchMapApp />
           </div>
           <div className="morefunc-aboutus-wrapper">
-            <img
-              src="http://via.placeholder.com/164x164"
-              alt="aboutus-logo"
-            />
-            <span>
-              만든 사람들 &#62;
-            </span>
+            <img src="http://via.placeholder.com/164x164" alt="aboutus-logo" />
+            <span>만든 사람들 &#62;</span>
           </div>
         </div>
       </div>
