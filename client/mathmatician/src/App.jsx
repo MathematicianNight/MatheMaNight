963eeeaf57baadaacf256bab497ae3b2a3243dbe
hyellokitty <hyelee1218@gmail.com>
2023-10-08 00:24:40 +0900

    App.jsx 수정

--- client/mathmatician/src/App.jsx
+++ client/mathmatician/src/App.jsx
@@ -2,38 +2,22 @@ import "./GlobalStyle.css";
 import "./assets/fonts/fonts.css";
 import { Route, Routes } from "react-router-dom";
 
-//Route
 import MainPage from "./pages/mainpage/page/index";
 import LinkKakaoCalendar from "./pages/kakaologin/page";
 import About from "./pages/about/page/index";
 import Hellokitty from "./pages/test/hellokitty";
 import Violet from "./pages/test/violet";
 
-//Images
-import { Images } from "./utils/style";
-
 function App() {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${Images.background_fix})`,
-        width: "100%",
-        minHeight: "100vh",
-        backgroundAttachment: "fixed",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <Routes style="background: ">
-        <Route path="/" element={<MainPage />} />
-        <Route path="/oauthkakao" element={<LinkKakaoCalendar />} />
-        <Route path="/hellokitty" element={<Hellokitty />} />
-        <Route path="/violet" element={<Violet />} />
-        <Route path="/aboutus" element={<About />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/oauthkakao" element={<LinkKakaoCalendar />} />
+      <Route path="/hellokitty" element={<Hellokitty />} />
+      <Route path="/violet" element={<Violet />} />
+      <Route path="/aboutus" element={<About />} />
+    </Routes>
   );
 }
 
 export default App;
-
