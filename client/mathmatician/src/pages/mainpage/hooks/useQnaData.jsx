4a100a29932be10d21bbaf10a012fcbe63c229e5
minjeoong <orange_mj@naver.com>
2023-10-08 16:04:18 +0900

    feat: 인사말 api 연결

--- client/mathmatician/src/pages/mainpage/hooks/useQnaData.jsx
+++ client/mathmatician/src/pages/mainpage/hooks/useQnaData.jsx
@@ -10,12 +10,10 @@ const useQnaData = (currentPage) => {
     const apiUrl = `/api?q=query&page=${currentPage}`; // 백엔드 엔드포인트에 맞게 수정
 
     fetch(apiUrl, {
-      method: "GET", // GET 요청을 사용하고 있지만 필요에 따라 다른 HTTP 메서드를 선택할 수 있습니다.
+      method: "GET",
       headers: {
-        "Content-Type": "application/json", // 데이터 형식을 JSON으로 설정
+        "Content-Type": "application/json",
       },
-      // 다른 옵션들도 설정할 수 있습니다.
-      // body: JSON.stringify(dataToSend), // 데이터를 JSON 문자열로 변환하여 보내는 예제
     })
       .then((response) => response.json())
       .then((data) => {
--- client/mathmatician/src/pages/mainpage/page/index.jsx
+++ client/mathmatician/src/pages/mainpage/page/index.jsx
@@ -3,26 +3,15 @@ import React, { useState } from "react";
 import { MainpageContainer } from "./styles";
 import QnaComponent from "../component/Qna/index";
 import Greetings from "../component/Greetings/index";
+import useGreetings from "../hooks/useGreetings";
 
 const MainPage = () => {
-  /** @todo 임시데이터 나중에 인사말 연결 **/
-  const message = `정보보안암호수학과를 이끌어 주시는
-재학생, 졸업생 여러분들 안녕하십니까
+  const message = useGreetings();
 
-벌써 2023년의 연말이 다가오고 있습니다.
-올해도 “제40회 수학인의 밤”을
-개최하여 한 해를 마무리하려고 합니다.
-
-소중한 시간을 빌리는 만큼
-좋은 행사로 찾아뵙겠습니다.
-자리에 참석하여 수학인의 밤을 빛내주세요.
-
-앞날을 항상 응원하겠습니다.
-감사합니다.
-
-제 7대 정보보안암호수학과
-학생회 “우린,” 올림`;
-  const messageLines = message.split("\n");
+  const messageLines =
+    message && message.length > 0
+      ? message.split("\n").filter((line) => line.trim() !== "")
+      : [];
 
   return (
     <MainpageContainer>
