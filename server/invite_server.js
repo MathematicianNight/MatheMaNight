e4a689e7f503a07b01982768024592ccd12f7190
LSuhyeon33 <gus989533@gmail.com>
2023-10-16 22:36:56 +0900

    cors change

--- server/invite_server.js
+++ server/invite_server.js
@@ -17,8 +17,8 @@ const options = {
   // origin: allowedOrigins, // 접근 권한을 부여하는 도메인
   // credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
   // optionsSuccessStatus: 200, // 응답 상태 200으로 설정
-  origin: allowedOrigins,
-  // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 };
 
