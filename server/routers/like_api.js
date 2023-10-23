46d37ee36162bf5c23f92fed4aba85c98eaab98c
LSuhyeon33 <gus989533@gmail.com>
2023-10-23 22:04:17 +0900

    make like table

--- server/routers/like_api.js
+++ server/routers/like_api.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const data = fs.readFileSync('./lib/db.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
 
-let likeCnt = 0;
-let overCnt = false;
+const db = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+db.connect();
+
+const cntViewsql = 'SELECT * FROM likecount; ';
+const cntUpsql =
+  'UPDATE likecount SET likeCnt = likeCnt + 1 WHERE likeCnt < 999; ';
 
 /**
  * @swagger
@@ -30,11 +44,14 @@ let overCnt = false;
  *                      example: false
  */
 router.get('/', (req, res) => {
-  const data = {
-    likeCnt: likeCnt,
-    overCnt: overCnt,
-  };
-  res.json(data);
+  db.query(cntViewsql, (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('데이터 조회 중 오류가 발생했습니다.');
+    } else {
+      res.json(result);
+    }
+  });
 });
 
 /**
@@ -49,15 +66,14 @@ router.get('/', (req, res) => {
  *        required: false
  */
 router.post('/upcount', (req, res) => {
-  if (likeCnt < 999) {
-    likeCnt++;
-    console.log('LikeCnt is upgrade!');
-    res.json('success!');
-  } else {
-    overCnt = true;
-    console.log('LikeCnt is over');
-    res.json('success! but LikeCnt is over');
-  }
+  db.query(cntUpsql, (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('데이터 입력 중 오류가 발생했습니다.');
+    } else {
+      res.json('success!');
+    }
+  });
 });
 
 module.exports = router;
